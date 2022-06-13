a983196c0a52bec7b0b4411edad93dfa61e31886
Guilherme Puida Moreira <guilhermepmoreira00@gmail.com>
2022-06-13 10:03:24 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/guilherme-puida/mks.lol
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+//go:embed content/index.tmpl
+var tmpl string
+
+var indexTemplate *template.Template
+
+// dataEntry is the main structure stored. It contains information about the links currently stored in the database.
+type dataEntry struct {
+	link      string
+	createdAt time.Time
+	expiresIn time.Duration
+}
+
+// renderData is passed to the template when rendering a new page. It contains data about the previous operation.
+type renderData struct {
+	// Represents a successful operation.
+	IsSuccess bool
+
+	// Contains the short url generated.
+	// Should only be set when IsSuccess is true.
+	ShortUrl string
+
+	// Represents an unsuccessful operation.
+	IsError bool
+
+	// Contains some sort of explanation about what went wrong.
+	// Should only be set when IsError is true.
+	ErrorMessage string
+
+	BaseUrl string
+}
+
+type serverOptions struct {
+	url   string
+	port  uint
+	https bool
+}
+
+// database is the data storage for the application. It maps a slug (a shortened link) to a dataEntry.
+var database map[string]dataEntry
+
+// durationMap maps the string representation of expiration times sent by the client in the form to an actual Go value.
+var durationMap map[string]time.Duration
+
+var options serverOptions
+
+func insertEntry(link string, expiresIn time.Duration) string {
+	var slug string
+
+	for {
+		slug = strconv.FormatUint(rand.Uint64(), 36)
+		_, ok := database[slug]
+		if !ok {
+			break
+		}
+	}
+
+	database[slug] = dataEntry{link: link, expiresIn: expiresIn, createdAt: time.Now()}
+	return slug
+}
+
+func generateShortUrl(slug string) string {
+	if options.https {
+		return fmt.Sprintf("https://%s/%s", options.url, slug)
+	}
+	return fmt.Sprintf("http://%s/%s", options.url, slug)
+
+}
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		switch r.URL.Path {
+		case "/":
+			indexTemplate.Execute(w, renderData{BaseUrl: options.url})
+		default:
+			entry, ok := database[strings.TrimPrefix(r.URL.Path, "/")]
+			if !ok {
+				indexTemplate.Execute(w, renderData{IsError: true, ErrorMessage: "Link not found in database.",
+					BaseUrl: options.url})
+				return
+			}
+			http.Redirect(w, r, entry.link, http.StatusMovedPermanently)
+		}
+	} else if r.Method == http.MethodPost {
+		switch r.URL.Path {
+		case "/":
+			r.ParseForm()
+			form := r.Form
+			link := form.Get("link")
+			expiresIn := form.Get("expiresIn")
+			slug := insertEntry(link, durationMap[expiresIn])
+			indexTemplate.Execute(w, renderData{IsSuccess: true, ShortUrl: generateShortUrl(slug),
+				BaseUrl: options.url})
+		default:
+			indexTemplate.Execute(w, renderData{IsError: true,
+				ErrorMessage: fmt.Sprintf("Method %s not allowed in URL %s", r.Method, r.URL.Path),
+				BaseUrl:      options.url})
+		}
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+// init is called before main, and used here to initialize all global variables.
+func init() {
+	// Initializing the random seed to the current time. This randomizes the values of the generated short URLs.
+	rand.Seed(time.Now().UnixNano())
+
+	// Initializing the database.
+	database = make(map[string]dataEntry)
+
+	// These are the expiration values that can be sent by the client.
+	durationMap = map[string]time.Duration{
+		"5min":  5 * time.Minute,
+		"15min": 15 * time.Minute,
+		"30min": 30 * time.Minute,
+		"1h":    1 * time.Hour,
+		"6h":    6 * time.Hour,
+		"12h":   12 * time.Hour,
+		"1day":  24 * time.Hour,
+	}
+
+	indexTemplate, _ = template.New("index").Parse(tmpl)
+}
+
+// deleteOldEntries handles deleting entries that have already expired.
+func deleteOldEntries() (deleted int) {
+
+	for k, v := range database {
+		if v.createdAt.Add(v.expiresIn).Before(time.Now()) {
+			deleted++
+			delete(database, k)
+		}
+	}
+
+	return deleted
+}
+
+func main() {
+	urlFlag := flag.String("url", "mks.lol", "url used in rendered templates")
+	portFlag := flag.Uint("port", 8080, "port that will listen for all requests")
+	httpsFlag := flag.Bool("https", false, "use https instead of http in rendered templates")
+
+	options = serverOptions{url: *urlFlag, port: *portFlag, https: *httpsFlag}
+
+	flag.Parse()
+
+	ticker := time.NewTicker(5 * time.Minute)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				deleted := deleteOldEntries()
+				log.Printf("Deleted %d entries. There are now %d entries in the database.\n", deleted, len(database))
+			}
+		}
+	}()
+
+	log.Printf("Service listening on port :%d.", options.port)
+	http.HandleFunc("/", requestHandler)
+	http.ListenAndServe(fmt.Sprintf(":%d", options.port), nil)
+}
